1538b194b9708277b14f9b612c3814dd7b39e113
Ryan Stewart <P22rstewart@fairfieldprep.org>
2021-11-03 10:02:27 -0400

    .

---
+++ hw5-5-2.py
@@ -0,0 +1,9 @@
+# Author RTS 11/3/21
+
+word = input("Enter a 6 letter word: ")
+
+odd = word[1] + "-" + word[3] + "-" + word[5]
+even = word[0] + "-" + word[2] + "-" + word[4]
+
+print(odd)
+print(even)
